2e2687b4366ec454147cf0db1b1215bc61fff0aa
KIWOONG KIM <55324587+kwnglab@users.noreply.github.com>
2025-02-02 22:56:16 +0900

    Julia 스터디

--- scripts/ibmhr/setup.jl
+++ scripts/ibmhr/setup.jl
@@ -22,3 +22,26 @@ end
 # # 결측치
 # nothing은 원래 없는 것
 # missing은 '있어야 할' 데이터가 없는 것
+
+using CSV
+using DataFrames
+using XLSX
+
+current_path = dirname(dirname(@__DIR__))
+println(current_path)
+
+# CSV 파일을 DataFrame으로 변환
+df_raw = CSV.File(joinpath(current_path, "data/raw/WA_Fn-UseC_-HR-Employee-Attrition.csv")) |> DataFrame
+
+# 엑셀 파일을 DataFrame으로 변환
+df_kor = XLSX.readtable(joinpath(current_path, "data/processed/ibmhr.xlsx"), "Sheet1") |> DataFrame
+
+# Column name 보는 법
+println(propertynames(df_kor))
+
+
+describe(df_kor)
+
+using StatsBase
+unique(df_kor.퇴직여부)
+countmap(df_kor.퇴직여부)
